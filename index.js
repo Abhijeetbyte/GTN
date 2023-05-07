937217636f421ea66c4eda5860448f18d69b0e40
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2023-05-07 19:43:16 +0530

    added a game story

--- index.js
+++ index.js
@@ -12,32 +12,35 @@ let user_num = 0; // store user input
 let rand_num = Math.floor(Math.random() * 101); // random number b/w 0 to 100
 
 //console.log(rand_num);  // for debugging
-console.log("Welcome to Guess the number game !\n")  // greetings
-user_num = prompt("Enter a number : "); // take an input form user
+console.log(" Welcome to Guess the Number Game!")  // greetings
+console.log("\n I am computer, and I have chosen a number between 0 - 100 ")  // about/story
+console.log("\n Now you have to guess it, let's begin ! \n ")  // about/story
+user_num = prompt(" Enter the number : "); // take an input from the user
 
 while (user_num != rand_num) {
 
   // Control structure
   if (user_num > rand_num) {  // check
-    console.log("\nTry again ! Hint : the number is smaller than", user_num, "\n");
+    console.log("\n Haa looser ! Hint : the number is smaller than", user_num, "\n");
     score++;  // increment by 1
-    user_num = prompt("Enter a number : "); // take an input form user
+    user_num = prompt(" Enter the number : "); // take an input form user
   }
 
   else if (user_num < rand_num) { // check
 
-    console.log("\nTry again ! Hint : the number is bigger than", user_num, "\n");
+    console.log("\n Haa looser ! Hint : the number is bigger than", user_num, "\n");
     score++;  // increment by 1
-    user_num = prompt("Enter a number : "); // take an input form user
+    user_num = prompt(" Enter the number : "); // take an input form user
   }
   else {
-    console.log("\nTry again ! Invalid input ", "\n");
-    user_num = prompt("Enter a number : ");
+    console.log("\n Haa looser ! Invalid input ", "\n");
+    user_num = prompt(" Enter the number : ");
   }
 }
 // exit loop
-console.log("\n***** Congratulations ! *****\n\nThe correct number is ", rand_num, "\nYour score is ", 100 - score);
-console.log("\nThanks for playing !\n"); // thanks for playing
+console.log("\n\n Damn it, you guessed it right !!! \n"); // 
+console.log("\n The correct number is ", rand_num, " Your score is ", 100 - score);
+console.log("\n I will see you next time...\n\n"); // thanks for playing
 
 
 
